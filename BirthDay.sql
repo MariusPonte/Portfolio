0e006ab6b4f2dc93f31d46650ae1b205fb33d355
MariusPonte <32370566+MariusPonte@users.noreply.github.com>
2017-10-20 11:15:13 +0200

    Update BirthDay.sql

--- BirthDay.sql
+++ BirthDay.sql
@@ -2,12 +2,12 @@
 
 -- It's your BirthDay!!
 
--- Calculating age can be a tricky thing. Usually,  a year has 365 days. But as we all know, not always. Sometimes there’s 366. And there lies the challenge in calculating age.
+-- Calculating age can be a tricky thing. Usually,  a year has 365 days. But as we all know, not always. Sometimes thereâ€™s 366. And there lies the challenge in calculating age.
 -- Most of the times, we see either a function that uses 365,25 days, which is a close estimate, but not 100% accurate. So when we want to congratulate our premium customers, being a day off is kind of amateuristic.
 -- So, we want to be accurate 100% of the time. That complicates things. 
--- Often used is logic that uses IF – THEN – ELSE reasoning. So if the current month > then month Date of birth OR (current month = month Date of birth AND DAY current month > then DAY Date of birth) THEN … ELSE …
+-- Often used is logic that uses IF â€“ THEN â€“ ELSE reasoning. So if the current month > then month Date of birth OR (current month = month Date of birth AND DAY current month > then DAY Date of birth) THEN â€¦ ELSE â€¦
 
--- Sounds like a lot of rules. What if there could be some magic that would solve this issue? Giving credit here to ‘dotjoe’ for his contribution on StackOverflow. Check out the following: 
+-- Sounds like a lot of rules. What if there could be some magic that would solve this issue? Giving credit here to â€˜dotjoeâ€™ for his contribution on StackOverflow. Check out the following: 
 -- Basically, we will display both date columns as numbers. Without going into details, it looks like this: 
 
 
@@ -16,7 +16,7 @@ USE MyDataBase
 
 SELECT LEFT (CAST (CONVERT (VARCHAR, GETDATE(), 112) AS INT) - CAST (CONVERT (VARCHAR, PERS_DoB, 112) AS INT), 2) AS Age
 
--- So, let’s say we have a table T_Person and we would like to find all people aged over 24.
+-- So, letâ€™s say we have a table T_Person and we would like to find all people aged over 24.
 -- Comparing an scalar function, using logic mentioned above to the inline select statement, would give this:
 
 SET STATISTICS TIME ON
@@ -37,3 +37,5 @@ WHERE DM.dbo.F_Age (PERS_DoB, GETDATE()) > 24
 
 
 -- In conclusion: Not only is this alternative way to calculate an age an elegant solution, it performs much, much better to conventional ways.
+
+
